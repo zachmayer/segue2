a61ba1bd3f155a9084cb05e6f43bca29484f76b8
James Long <jdlong@gmail.com>
2010-12-17 17:00:25 -0600

    added log4j logging.

--- R/zzz.R
+++ R/zzz.R
@@ -1,24 +1,23 @@
 
 ##' @import rJava
 .onLoad <- function(lib, pkg) {
-
     pathToSdk <- paste(system.file(package = "segue") , "/aws-java-sdk/", sep="")
 
     jarPaths <- c(paste(pathToSdk, "lib/aws-java-sdk-1.1.0.jar", sep=""),
                   paste(pathToSdk, "third-party/commons-logging-1.1.1/commons-logging-1.1.1.jar", sep=""),
                   paste(pathToSdk, "third-party/commons-httpclient-3.0.1/commons-httpclient-3.0.1.jar", sep=""),
                   paste(pathToSdk, "third-party/commons-codec-1.3/commons-codec-1.3.jar", sep=""),
-                  paste(pathToSdk, "third-party/jets3t-0.8.0.jar", sep="")
+                  paste(pathToSdk, "third-party/log4j-1.2.16.jar", sep=""),
+                  paste(pathToSdk, "third-party/", sep="")
                   )
     .jpackage(pkg, morePaths=jarPaths)
-    # attach( javaImport( c("java.lang", "java.io"), pos = length( search() ), name = "java" ))
     attach( javaImport( c("java.lang", "java.io")))
     
     if (Sys.getenv("AWSACCESSKEY") != "" && Sys.getenv("AWSSECRETKEY") != ""){
       awsCreds <- new(com.amazonaws.auth.BasicAWSCredentials, Sys.getenv("AWSACCESSKEY"), Sys.getenv("AWSSECRETKEY"))
       assign("awsCreds", awsCreds, envir = .GlobalEnv)
       packageStartupMessage( "Segue has loaded your AWS Credentials." )
-     } else {
-       packageStartupMessage( "Segue did not find your AWS credentials." )
-     }
+    } else {
+       packageStartupMessage( "Segue did not find your AWS credentials. Please run the setCredentials() function." )
+    }
 }
