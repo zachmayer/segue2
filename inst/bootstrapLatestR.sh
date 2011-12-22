3530319da1c282e4842ba69666ed57b017e417ab
James Long <jal@renre.com>
2011-12-22 14:01:22 -0600

    rolled in EMR spot instance changes as well as commenting out key info in the bootstrap to prevent error.

--- inst/bootstrapLatestR.sh
+++ inst/bootstrapLatestR.sh
@@ -40,9 +40,9 @@ echo "force-confold" | sudo tee -a  /etc/dpkg/dpkg.cfg
 echo "force-confdef" | sudo tee -a  /etc/dpkg/dpkg.cfg
 
 # add key to keyring so it doesn't complain
-gpg --keyserver pgp.mit.edu --recv-key 381BA480
-gpg -a --export 381BA480 > jranke_cran.asc
-sudo apt-key add jranke_cran.asc
+# gpg --keyserver pgp.mit.edu --recv-key 381BA480
+# gpg -a --export 381BA480 > jranke_cran.asc
+# sudo apt-key add jranke_cran.asc
 
 # install the gfortran
 sudo apt-get update
