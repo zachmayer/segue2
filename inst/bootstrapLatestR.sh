b59879543936b117e0b38ae5136cdf3616174aa8
James Long <jal@renre.com>
2012-03-27 21:24:35 -0400

    updated the bootstrapLatestR.sh to account for Squeeze (instead of Lenny) being the default Debian on EMR

--- inst/bootstrapLatestR.sh
+++ inst/bootstrapLatestR.sh
@@ -5,35 +5,37 @@ set -e -x
 
 # Default to stable
 sudo tee /etc/apt/sources.list <<EOF
-deb http://http.us.debian.org/debian   lenny          main contrib non-free
+deb http://http.us.debian.org/debian   squeeze          main contrib non-free
 deb http://http.us.debian.org/debian   stable         main contrib non-free
-deb http://security.debian.org         lenny/updates  main contrib non-free
+deb http://security.debian.org         squeeze/updates  main contrib non-free
 deb http://security.debian.org         stable/updates main contrib non-free
 
 # Change these lines if you don't want to use the main CRAN mirror.
 # debian R upgrade
-deb http://cran.r-project.org/bin/linux/debian lenny-cran/
-deb-src http://cran.r-project.org/bin/linux/debian lenny-cran/
-deb http://cran.r-project.org/bin/linux/debian lenny-cran/
-deb-src http://cran.r-project.org/bin/linux/debian lenny-cran/
+deb http://cran.r-project.org/bin/linux/debian squeeze-cran/
+deb-src http://cran.r-project.org/bin/linux/debian squeeze-cran/
+deb http://cran.r-project.org/bin/linux/debian squeeze-cran/
+deb-src http://cran.r-project.org/bin/linux/debian squeeze-cran/
 EOF
-sudo tee /etc/apt/preferences <<EOF
-Package: *
-Pin: release a=oldstable
-Pin-Priority: 910
 
-Package: *
-Pin: release a=stable
-Pin-Priority: 920
+## THE FOLLOWING WAS NEEDED IN LENNY, BUT IT APPEARS NOT IN SQUEEZE
+# sudo tee /etc/apt/preferences <<EOF
+# Package: *
+# Pin: release a=oldstable
+# Pin-Priority: 910
 
-Package: *
-Pin: release a=testing
-Pin-Priority: 900
+# Package: *
+# Pin: release a=stable
+# Pin-Priority: 920
 
-Package: *
-Pin: release a=unstable
-Pin-Priority: 800
-EOF
+# Package: *
+# Pin: release a=testing
+# Pin-Priority: 900
+
+# Package: *
+# Pin: release a=unstable
+# Pin-Priority: 800
+# EOF
 
 ## test
 echo "force-confold" | sudo tee -a  /etc/dpkg/dpkg.cfg
@@ -46,7 +48,8 @@ echo "force-confdef" | sudo tee -a  /etc/dpkg/dpkg.cfg
 
 # install the gfortran
 sudo apt-get update
-sudo apt-get install --yes gfortran-4.2
+## when we went to Squeeze from lenny, 4.2 no longer installs. Is it needed? time will tell!! 
+## sudo apt-get install --yes gfortran-4.2
 
 # install R using the FRONTEND call to eliminate
 # user interactive requests
@@ -63,16 +66,6 @@ sudo env JAVA_HOME=/usr/lib/jvm/java-6-sun/jre R CMD javareconf
 
 #install littler
 sudo apt-get install littler
-
-#the apt-get install does not work right on 64 bit... so building from source.
-#cd /home/hadoop
-#wget http://dirk.eddelbuettel.com/code/littler/littler-0.1.3.tar.gz
-#tar zxvf littler-0.1.3.tar.gz
-#cd littler-0.1.3
-#./configure
-#make
-#sudo make install
-
 
 #some packages have trouble installing without this link
 sudo ln -s /usr/lib/libgfortran.so.3 /usr/lib/libgfortran.so
