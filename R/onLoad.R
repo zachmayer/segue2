ee8c5d533b363486c915510291b4640035bca4f6
Zach Mayer <zach.mayer@gmail.com>
2015-02-05 16:48:36 -0500

    starting to use options for AWS creeds

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,14 +2,12 @@
 
   .jpackage(pkgname, lib.loc = libname)
 
+  #TODO: use get and set options
   if (Sys.getenv("AWSACCESSKEY") != "" && Sys.getenv("AWSSECRETKEY") != ""){
     awsCreds <- new(com.amazonaws.auth.BasicAWSCredentials, Sys.getenv("AWSACCESSKEY"), Sys.getenv("AWSSECRETKEY"))
-    assign("awsCreds", awsCreds, envir = .GlobalEnv)
+    options(awsCreds = awsCreds)
     packageStartupMessage( "Segue has loaded your AWS Credentials." )
   } else {
     packageStartupMessage( "Segue did not find your AWS credentials. Please run the setCredentials() function." )
   }
-
-
-  .jpackage(pkgname, lib.loc = libname)
 }
