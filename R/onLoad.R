23ab6a36fcd3b11fd740003a75e1905bd78f89b5
Zach Mayer <zach.mayer@gmail.com>
2015-02-05 16:46:19 -0500

    Revert "jaaavaaaa"
    This reverts commit 09d027175b1a40ca2b0180b36f2a28794c1a71d2.

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,15 @@
 .onLoad <- function(libname, pkgname) {
+
+  .jpackage(pkgname, lib.loc = libname)
+
+  if (Sys.getenv("AWSACCESSKEY") != "" && Sys.getenv("AWSSECRETKEY") != ""){
+    awsCreds <- new(com.amazonaws.auth.BasicAWSCredentials, Sys.getenv("AWSACCESSKEY"), Sys.getenv("AWSSECRETKEY"))
+    assign("awsCreds", awsCreds, envir = .GlobalEnv)
+    packageStartupMessage( "Segue has loaded your AWS Credentials." )
+  } else {
+    packageStartupMessage( "Segue did not find your AWS credentials. Please run the setCredentials() function." )
+  }
+
+
   .jpackage(pkgname, lib.loc = libname)
 }
