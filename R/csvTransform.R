12ea257a97197c872cc6b5dbcd985b88b5de5208
Zach Mayer <zach.mayer@gmail.com>
2015-02-05 12:45:11 -0500

    Correctly import from caTools

--- R/csvTransform.R
+++ R/csvTransform.R
@@ -1,27 +1,24 @@
 ##' Converts an R list into a text CSV.
 ##' Serializes each element of an R list into ASCII characters then encodes then
 ##' for use as the input to a Hadoop Streaming job.
-##' 
+##'
 ##' @param inList list to convert to CSV
 ##' @param outFileName file name of resulting CSV file
-##' @return creates a CSV to file but returns nothing. 
+##' @return creates a CSV to file but returns nothing.
 ##' @author James "JD" Long
 ##' @seealso csvToList
-##' 
-##' 
 ##' @export
-listToCsv <-
-function(inList, outFileName){
-  #require(caTools)
-  if (is.list(inList) == FALSE) 
-        stop("listToCsv: The input list fails the is.list() check.")
+##' @importFrom caTools base64encode
+listToCsv <- function(inList, outFileName){
+  if (is.list(inList) == FALSE)
+    stop("listToCsv: The input list fails the is.list() check.")
   fileName <- outFileName
   cat("", file=fileName, append=FALSE)
-  
+
   i <- 1
   for (item in inList) {
     myLine <- paste(i, ",", base64encode(serialize(item, NULL, ascii=TRUE)), "\n", sep="")
-    cat(myLine, file=fileName, append=TRUE) 
+    cat(myLine, file=fileName, append=TRUE)
     i <- i+1
   }
 }
@@ -29,8 +26,8 @@ function(inList, outFileName){
 ##' The inverse of the listToCsv() function
 ##' Takes a csv of serialized objects created by the listToCSV function and
 ##' turns it into a proper R list.
-##' 
-##' 
+##'
+##'
 ##' @param inFileName String pointing to the full path of the input CSV.
 ##' @return Returns a list object. Or an error. Hopefully a list.
 ##' @author James "JD" Long
@@ -42,17 +39,17 @@ function(inList, outFileName){
 ##'     a <- c(rnorm(999), NA)
 ##'     myList[[i]] <- a
 ##'   }
-##' 
+##'
 ##'   require(caTools)
 ##'   listToCsv(myList, "tst.csv")
 ##'   all.equal(myList,  csvToList("tst.csv" ))
-##' 
+##'
 ##' @export
+##' @importFrom caTools base64decode
 csvToList <- function(inFileName){
-  #require(caTools)
   linesIn <- readLines(inFileName, n=-1)
   outList <- NULL
-  
+
   i <- 1
   for (line in linesIn){
     outList[[i]] <- unserialize(base64decode(strsplit(linesIn[[i]], split=",")[[1]][[2]], "raw"))
@@ -60,4 +57,3 @@ csvToList <- function(inFileName){
   }
   return(outList)
 }
-
