b6ea37c575405dcfc3bc21eb9c0e01c0635ebebb
Zach Mayer <zach.mayer@gmail.com>
2015-02-05 12:27:24 -0500

    Started the switch to RAmazonS3
    I’m also hoping we can use the hadoop rest api to submit jobs and avoid
    java 100%

--- R/segue-package.R
+++ R/segue-package.R
@@ -6,21 +6,18 @@
 ##' package. If you are CPU bound this might be a useful package.
 ##' "We are stuck with technology when what we really want is just stuff that
 ##' works." - Douglas Adams
-##' 
+##'
 ##' \tabular{ll}{ Package: \tab segue\cr Type: \tab Package\cr Version: \tab
 ##' 0.01\cr Date: \tab 2010-12-08\cr License: \tab MIT License \cr LazyLoad:
 ##' \tab yes\cr } ~~ An overview of how to use the package, including the most
 ##' important ~~ ~~ functions ~~
-##' 
+##'
 ##' @name segue-package
 ##' @docType package
-##' @author Maintainer: James "JD" Long
-##' @references http://code.google.com/p/segue/
+##' @author Maintainer: Zachary Mayer
+##' @references https://github.com/zachmayer/segue2
 ##' @keywords package
-##' 
-##' 
-##' 
+##'
+##'
+##'
 NULL
-
-
-
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 [![Build Status](https://travis-ci.org/zachmayer/segue2.png?branch=master)](https://travis-ci.org/zachmayer/segue2)
 [![Coverage Status](https://coveralls.io/repos/zachmayer/segue2/badge.svg)](https://coveralls.io/r/zachmayer/segue2)
-### Segue2: abusing Amazon Elastic Map reduce for embarrassingly parallel R jobs
+### Segue2: 
+##Abusing Amazon Elastic Map reduce for embarrassingly parallel R jobs
+Note that this package depends on [RAmazonS3](http://www.omegahat.org/RAmazonS3/), which is not on CRAN:
+```{R}
+install.packages("RAmazonS3", repos="http://www.omegahat.org/R", type="source")
+```
